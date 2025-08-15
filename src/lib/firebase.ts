4b45325795ebeceea01a8bd29a99b5c4e0890c00
Sulman Bashir <sulmanbashirgujrat777@gmail.com>
2025-08-15 23:35:14 +0000

    yar error nahi colve horaha may nay tumay doora her cheex data ho tum ch

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,16 @@
+
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration provided by you.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyCcC8nJRx4DekFI55O609zpohh-Br0OD1o",
+  authDomain: "sulzx-38b13.firebaseapp.com",
+  projectId: "sulzx-38b13",
+  storageBucket: "sulzx-38b13.appspot.com",
+  messagingSenderId: "425569249415",
+  appId: "1:425569249415:web:9067543f029988493f7022",
+  measurementId: "G-TV60QB8118"
 };
 
 // Initialize Firebase

10f96bbed4912fa973fc5f9c7910f4519e822434
Sulman Bashir <sulmanbashirgujrat777@gmail.com>
2025-08-15 23:26:55 +0000

    FirebaseError: Firebase: Error (auth/invalid-api-key).     at createErro

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,6 @@ const firebaseConfig: FirebaseOptions = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
 const auth = getAuth(app);
 
 export { app, auth };

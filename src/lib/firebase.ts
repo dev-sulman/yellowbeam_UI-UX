bc85c4cc110a2af468ae2b6a50e4174d5893902e
Sulman Bashir <sulmanbashirgujrat777@gmail.com>
2025-08-15 20:14:22 +0000

    y to kab kay add kar deya h

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,6 +11,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 

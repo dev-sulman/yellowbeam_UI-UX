f5cb51079ff9a0e0fd4b390132e5b617e682b8b8
Sulman Bashir <sulmanbashirgujrat777@gmail.com>
2025-09-28 23:39:17 +0000

    is ka sise or choots karo (_for element img_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -123,11 +123,11 @@ export default function Footer() {
       {isVisible && (
         <Button 
             onClick={scrollToTop} 
-            className="fixed bottom-8 right-5 h-12 w-12 rounded-full bg-background/80 backdrop-blur-sm shadow-lg hover:bg-slate-950 hover:text-white border text-foreground"
+            className="fixed bottom-8 right-5 h-10 w-10 rounded-full bg-background/80 backdrop-blur-sm shadow-lg hover:bg-slate-950 hover:text-white border text-foreground"
             variant="outline"
             size="icon"
         >
-            <ChevronUp className="h-6 w-6" />
+            <ChevronUp className="h-5 w-5" />
         </Button>
       )}
     </footer>
